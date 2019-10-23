7ba47f0246b6281d1fb7fdb1628e7a70e6ffd42f
Andrius Aucinas <aaucinas@brave.com>
2019-10-23 09:02:12 +0100

    name-based test groups

--- memory-windows.ps1
+++ memory-windows.ps1
@@ -7,24 +7,31 @@ Write-Output "Measuring $browser for scenario $test"
 
 $repeats = 3
 $wait = 30
+$braveapplication = '\BraveSoftware\Brave-Browser-Beta\Application\'
 
 for ($i=1; $i -le $repeats; $i++) {
-    Start-Process -FilePath $browser -WorkingDirectory $ENV:LOCALAPPDATA'\BraveSoftware\Brave-Browser-Beta\Application\'
+    Start-Process -FilePath $browser -WorkingDirectory $ENV:LOCALAPPDATA$braveapplication
     Start-Sleep -Seconds $wait
 
-    Get-Content .\scenarios\$test.txt | ForEach-Object {
-        $page = $_
-        Write-Output "Opening page $page"
-        Start-Process -FilePath $browser -WorkingDirectory $ENV:LOCALAPPDATA'\BraveSoftware\Brave-Browser-Beta\Application\' -ArgumentList $page
+    Get-ChildItem ".\scenarios\" -Filter $test*.log | 
+    Foreach-Object {
+        $fullname = $_.FullName
+        $test = $_.Name
+        Get-Content $fullname | ForEach-Object {
+            $page = $_
+            Write-Output "Opening page $page"
+            Start-Process -FilePath $browser -WorkingDirectory $ENV:LOCALAPPDATA$braveapplication -ArgumentList $page
+            Start-Sleep -Seconds 5
+        }
+    
+
+        Start-Sleep -Seconds $wait
+
+        $m = ps $browser | measure PM -Sum
+
+        ("$browser $test $i {0:N2}MB " -f ($m.sum / 1mb))
+
+        Get-Process -Name $browser | Foreach-Object { $_.CloseMainWindow() | Out-Null } | Stop-Process
         Start-Sleep -Seconds 5
     }
-
-    Start-Sleep -Seconds $wait
-
-    $m = ps $browser | measure PM -Sum
-
-    ("$browser $test {0:N2}MB " -f ($m.sum / 1mb))
-
-    Get-Process -Name $browser | Foreach-Object { $_.CloseMainWindow() | Out-Null } | Stop-Process
-    Start-Sleep -Seconds 5
 }

678dbaa23c1f5e3b780bf45cfbc896b97facb92e
Andrius Aucinas <aaucinas@brave.com>
2019-10-30 14:00:00 +0000

    point directly at opera. todo: do not hardcode version

--- sitespeed-bench.ps1
+++ sitespeed-bench.ps1
@@ -14,19 +14,19 @@ Get-Content $test | ForEach-Object {
 				$spbrowser="chrome"
 				$binaryFlag="--$spbrowser.binaryPath"
 				$binaryPath="$ENV:LOCALAPPDATA\BraveSoftware\Brave-Browser-Beta\Application\brave.exe"
-				$driver="--chrome.chromeDriverPath=chromedrivers/win/chromedriver.exe"
+				$driver="--chrome.chromedriverPath=chromedrivers/win/chromedriver.exe"
 			}
 			"Chrome" {
 				$spbrowser="chrome"
 				$binaryFlag="--$spbrowser.binaryPath"
 				$binaryPath="${Env:Programfiles(x86)}\Google\Chrome Beta\Application\chrome.exe"
-				$driver="--chrome.chromeDriverPath=chromedrivers/win/chromedriver.exe"
+				$driver="--chrome.chromedriverPath=chromedrivers/win/chromedriver.exe"
 			}
 			"Opera" {
 				$spbrowser="chrome"
 				$binaryFlag="--$spbrowser.binaryPath"
-				$binaryPath="$ENV:LOCALAPPDATA\Programs\Opera\launcher.exe"
-				$driver="--chrome.chromeDriverPath=chromedrivers/win/operadriver.exe"
+				$binaryPath="$ENV:LOCALAPPDATA\Programs\Opera\64.0.3417.73\opera.exe"
+				$driver="--chrome.chromedriverPath=chromedrivers/win/operadriver.exe"
 			}
 			"Firefox" {
 				$spbrowser="firefox"

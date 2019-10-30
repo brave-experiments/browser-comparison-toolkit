1495b7aaf50401b4b3cbf9026dbe9d00b35d278a
Andrius Aucinas <aaucinas@brave.com>
2019-10-30 09:15:59 +0000

    single-pass data collection script for all browsertime results

--- collect.sh
+++ collect.sh
@@ -1,36 +1,41 @@
 dir=$1
-browser=$2
-scenario=$3
+scenario=$2
+output=$3
 
 (echo "["
-for file in $dir/$browser/$scenario/*/browsertime.json
+for BROWSER in Chrome Brave Firefox Opera
 do
+    for file in $dir/$BROWSER/$scenario/*/browsertime.json
+    do
+        har=$(dirname "$file")/$(basename "$file" .json).har
+        browsertime=$file
+        if [[ -f "$browsertime" ]] && [[ -f "$har" ]]; then
+            #statements
+            (
+                jq " .[] | {
+                    browser: \"$BROWSER\",
+                    url: .info .url,
+                    timestamp: .info .timestamp,
+                    rumSpeedIndex: .statistics .timings .rumSpeedIndex | {median: .median, mean: .mean, stddev: .stddev},
+                    firstPaint: .statistics .timings .firstPaint | {median: .median, mean: .mean, stddev: .stddev},
+                    loadEvent: .statistics .timings .loadEventEnd | {median: .median, mean: .mean, stddev: .stddev},
+                    fullyLoaded: .statistics .timings .fullyLoaded | {median: .median, mean: .mean, stddev: .stddev}
+                }" $browsertime;
+                jq ".log | {
+                    sizes: .entries 
+                        | group_by(.pageref)
+                        | map({
+                              (.[0].pageref): [
+                                    (map(.response | ([.headersSize, .bodySize] | add)) | add),
+                                    (map(.request | ([.headersSize, .bodySize] | add)) | add)
+                                ] | add
+                          })
+                        | add
+                  }" $har
+            ) | jq -n '[inputs] | add'
+            echo ","
+        fi
 
-    (
-        jq " .[] | {
-            browser: \"$browser\",
-            url: .info .url,
-            timestamp: .info .timestamp,
-            rumSpeedIndex: .statistics .timings .rumSpeedIndex | {median: .median, mean: .mean, stddev: .stddev},
-            firstPaint: .statistics .timings .firstPaint | {median: .median, mean: .mean, stddev: .stddev},
-            loadEvent: .statistics .timings .loadEventEnd | {median: .median, mean: .mean, stddev: .stddev},
-            fullyLoaded: .statistics .timings .fullyLoaded | {median: .median, mean: .mean, stddev: .stddev}
-        }" $file;
-        jq ".log | {
-            sizes: .entries 
-                | group_by(.pageref)
-                | map({
-                      (.[0].pageref): [
-                            (map(.response | ([.headersSize, .bodySize] | add)) | add),
-                            (map(.request | ([.headersSize, .bodySize] | add)) | add)
-                        ] | add
-                  })
-                | add
-          }" $(dirname "$file")/$(basename "$file" .json).har
-    ) | jq -n '[inputs] | add'
-    echo ","
-
+    done
 done
-echo "]") > $browser-$scenario.json
-
-
+echo "]") | json2csv --flatten > $output

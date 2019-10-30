1f67aaa59319fd8bf9b6c7eb2648991cacdd8e6f
Andrius Aucinas <aaucinas@brave.com>
2019-10-30 12:18:58 +0000

    setting up remaining browsers for windows testing

--- sitespeed-bench.ps1
+++ sitespeed-bench.ps1
@@ -8,22 +8,36 @@ switch ($browser)
 {
 	"Brave" {
 		$spbrowser="chrome"
-		$binaryPath="$ENV:LOCALAPPDATA\BraveSoftware\Brave-Browser-Beta\Application\"
-		$driver="--chrome.chromeDriverPath=chromedrivers/win32/chromedriver.exe"
+		$binaryFlag="--$spbrowser.binaryPath"
+		$binaryPath="$ENV:LOCALAPPDATA\BraveSoftware\Brave-Browser-Beta\Application\brave.exe"
+		$driver="--chrome.chromeDriverPath=chromedrivers/win/chromedriver.exe"
 	}
 	"Chrome" {
 		$spbrowser="chrome"
-		$binaryPath="chrome.exe"
-		$driver="--chrome.chromeDriverPath=chromedrivers/win32/chromedriver.exe"
+		$binaryFlag="--$spbrowser.binaryPath"
+		$binaryPath="${Env:Programfiles(x86)}\Google\Chrome Beta\Application\chrome.exe"
+		$driver="--chrome.chromeDriverPath=chromedrivers/win/chromedriver.exe"
+	}
+	"Opera" {
+		$spbrowser="chrome"
+		$binaryFlag="--$spbrowser.binaryPath"
+		$binaryPath="$ENV:LOCALAPPDATA\Programs\Opera\launcher.exe"
+		$driver="--chrome.chromeDriverPath=chromedrivers/win/operadriver.exe"
+	}
+	"Firefox" {
+		$spbrowser="firefox"
+		$binaryFlag=""
+		$binaryPath=""
+		$driver=""
 	}
 }
 
-browsertime -b $spbrowser --$spdriver.binaryPath $binaryPath $driver`
-	-n 3 `
+browsertime -b $spbrowser $binaryFlag $binaryPath $driver `
+	--iterations 3 `
 	--pageCompleteCheckInactivity `
 	--resultDir browsertime/$BROWSER/4g/$i `
 	--viewPort maximize `
 	--connectivity.alias unthrottled `
 	$url
-				
+
 

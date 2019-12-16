b4e8fc1cc5e13e16bbb15fb4fdb9554e742af4a6
Andrius Aucinas <aaucinas@brave.com>
2019-12-16 10:32:20 +0000

    fixes local install path

--- memory-bench.ps1
+++ memory-bench.ps1
@@ -10,7 +10,7 @@ $wait = 30
 $braveapplication = '\BraveSoftware\Brave-Browser\Application\'
 $localinstall = Test-Path -Path $ENV:LOCALAPPDATA$braveapplication
 if ($localinstall) {
-    $workingdir = "-WorkingDirectory $localinstall"
+    $workingdir = "-WorkingDirectory $braveapplication"
 }
 
 $userdatadir = "$pwd\mem-test\"
